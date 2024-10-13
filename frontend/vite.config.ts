31b92e2dcfd9a742b8bcfc90dfa10e0af0e1f76e
Dawid Komęza <dawid.komeza@gmail.com>
2024-10-13 22:58:19 +0200

    🔧 Add proxy to vite dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,13 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3000,
     strictPort: true,
+    proxy: {
+      "/api/": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   resolve: {
     alias: {
