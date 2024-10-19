0a6f11b35e5221fe9e92addc392af6c87e3cb6a4
Dawid Komęza <dawid.komeza@gmail.com>
2024-10-19 20:34:28 +0200

    🎉 This time it is really finished

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,8 +1,6 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-console.log("DB_HOST", process.env.DB_HOST);
-
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST || "localhost",
@@ -13,7 +11,4 @@ export const AppDataSource = new DataSource({
   synchronize: true,
   logging: false,
   entities: [__dirname + "/../models/*.model.ts"],
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
